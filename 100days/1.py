4e30d6d2c120a911c8e6cc1f0918128bf7fb372b
TophNanfong <TophTab@github.com>
2020-11-07 21:34:55 +0800

    21.34

--- 100days/1.py
+++ 100days/1.py
@@ -1,10 +1,18 @@
 for i in range(1, 10):
     for j in range(1, i + 1):
-        print('%d*%d=%d' % (i, j, i * j), end='\t')#end表示结尾不换行
+        print('%d*%d=%d' % (i, j, i * j), end='\t')#end表示结尾不换行，\t表示4个空格
     print()
 
 for x in range(1,10):
     for y in range(1,x+1):
         z=x*y
-        print(str(x)+'*'+str(y)+'='+str(z)+' \t')
+        tap=str(x)+'*'+str(y)+'='+str(z)
+        print(tap,end='')
+    print()
+
+for x in range(1,10):
+    for y in range(1,x+1):
+        z=x*y
+        tap=str(x)+'*'+str(y)+'='+str(z)
+        print(tap,end=' ')
     print()
